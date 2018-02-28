05d8cb6f9b5bffb83c367350cac3c88e195e6227
jieyuchongliang <1134119088@qq.com>
2018-02-28 14:20:24 +0800

    Initial commit

---
+++ app/src/main/java/fragmentdemo/fujisoft/com/dialogactivity/MainActivity.java
@@ -0,0 +1,21 @@
+package fragmentdemo.fujisoft.com.dialogactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_show_dialogactivity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,DialogActivity.class));
+            }
+        });
+    }
+}
